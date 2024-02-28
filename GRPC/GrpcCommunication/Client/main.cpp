7edb61c214c260c8520537b2b5b408fbcd001de7
indir <indir.karabegovic@hotmail.com>
2024-02-28 16:02:53 +0100

    Preparing GRPC project

---
+++ GRPC/GrpcCommunication/Client/main.cpp
@@ -0,0 +1,14 @@
+//#include "Client.h"
+#include <iostream>
+
+
+int main(int argc, char* argv[])
+{
+    //std::cout<<"Prepaing client info"<<std::endl;
+    //ClientInfo clientInformations;
+    //clientInformations.SetClientInfo();
+    //std::cout<<"Client prepared"<<std::endl;
+    //GrpcClient client{"localhost:50051", clientInformations};
+    //client.SendMessages();
+    return 0;
+}
---
+++ GRPC/GrpcCommunication/Server/main.cpp
@@ -0,0 +1,18 @@
+//#include "Server.h"
+
+
+int main(int argc, char* argv[])
+{
+//     grpc::ServerBuilder builder;
+//     builder.AddListeningPort("0.0.0.0:50051", grpc::InsecureServerCredentials());
+
+//     ServerInformation basicInfo{"MedicalServerForBasicOperations", 196547};
+//     GrpcServer grpcServer{basicInfo};
+//     MedDataExchangeService my_service;
+//     builder.RegisterService(&grpcServer);
+
+//     std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+//     std::cout<<"Grpc Server started" <<std::endl;
+//     server->Wait();
+    return 0;
+}
