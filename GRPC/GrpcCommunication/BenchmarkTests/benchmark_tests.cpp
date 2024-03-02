511d52cdf4ff072bb6def9cda393a5cf6a3a6262
indir <indir.karabegovic@hotmail.com>
2024-03-02 12:58:27 +0100

    Some minor changes

--- GRPC/GrpcCommunication/BenchmarkTests/benchmark_tests.cpp
+++ GRPC/GrpcCommunication/BenchmarkTests/benchmark_tests.cpp
@@ -24,6 +24,7 @@ static void BM_GrpcMessageCreationProbe(benchmark::State& state) {
     std::string clientType{"type"};
     for (auto _ : state) {
         ProbeMsg probe = PrepareProbeData(0, clientName, clientType);
+        std::string example{probe.SerializeAsString()};
     }
 }
 // Register the function as a benchmark
@@ -33,6 +34,7 @@ static void BM_GrpcMessageCreationProbeMatch(benchmark::State& state) {
     std::string serverName{"name"};
     for (auto _ : state) {
         ProbeMsgResponse probe = PrepareProbeMatchData(0, true, serverName);
+        std::string example{probe.SerializeAsString()};
     }
 }
 // Register the function as a benchmark
@@ -42,10 +44,13 @@ static void BM_GrpcMessageParsingProbe(benchmark::State& state) {
     std::string clientName{"name"};
     std::string clientType{"type"};
     ProbeMsg probe = PrepareProbeData(0, clientName, clientType);
+    std::string example{probe.SerializeAsString()};
     for (auto _ : state) {
-        std::string parsedName{probe.medicaldevicename()};
-        std::string parsedType{probe.medicaldevicetype()};
-        int parsedId{probe.medicaldeviceid()};
+        ProbeMsg newProbe;
+        newProbe.ParseFromString(example);
+        std::string parsedName{newProbe.medicaldevicename()};
+        std::string parsedType{newProbe.medicaldevicetype()};
+        int parsedId{newProbe.medicaldeviceid()};
     }
 }
 // Register the function as a benchmark
@@ -54,27 +59,16 @@ BENCHMARK(BM_GrpcMessageParsingProbe);
 static void BM_GrpcMessageParsingProbeMatch(benchmark::State& state) {
     std::string serverName{"name"};
     ProbeMsgResponse probe = PrepareProbeMatchData(0, true, serverName);
+    std::string example{probe.SerializeAsString()};
     for (auto _ : state) {
-        std::string parsedName{probe.medicalservername()};
-        int parsedid{probe.medicalserverid()};
-        bool flag{probe.probematch()};
+        ProbeMsgResponse newProbe;
+        newProbe.ParseFromString(example);
+        std::string parsedName{newProbe.medicalservername()};
+        int parsedid{newProbe.medicalserverid()};
+        bool flag{newProbe.probematch()};
     }
 }
 // Register the function as a benchmark
 BENCHMARK(BM_GrpcMessageParsingProbeMatch);
-
-//static void BM_SoapMessageParsingProbeMatch(benchmark::State& state) {
-//    Domain::InitializeLibrary();
-//    auto message{Domain::PrepareProbeMatchMessage()};
-//    for (auto _ : state) {
-//        Domain::XmlParser parser{};
-//        parser.ParseMessage(message);
-//        Domain::ProbeMatchData probe;
-//        parser.GetProbeMatchData(probe);
-//    }
-//    Domain::DeinitializeLibrary();
-//}
-//// Register the function as a benchmark
-//BENCHMARK(BM_SoapMessageParsingProbeMatch);
 
 BENCHMARK_MAIN();
